4bc316b9f329127a631b46a823fc664aea2cc659
tirenton <128995795+tirenton@users.noreply.github.com>
2024-02-29 15:09:06 +0700

    Update ก้านกล้วย.cpp

--- ก้านกล้วย.cpp
+++ ก้านกล้วย.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+    vector<int> lst(n + 1, 0);
+    vector<int> dp(n + 1, 0);
+    
+    for (int i = 1; i <= n; i++){
+        cin >> lst[i];
+        if (i>2) dp[i] = max(dp[i-3]+lst[i],max(dp[i-1],dp[i-2]));
+    }
+    
+    cout << dp[n] << endl;
+    return 0;
+}
